b8735d73f7b33f29dc41dd7a64d79f52960f1f51
Sanket Shevkar <shevkar.sanket@gmail.com>
2021-06-02 22:54:06 +0530

    chore(pages): pages setup
    Signed-off-by: Sanket Shevkar <shevkar.sanket@gmail.com>

---
+++ pages/experience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function experience() {
+    return (
+        <div>
+            Experience
+        </div>
+    )
+}
+
+export default experience
---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function projects() {
+    return (
+        <div>
+            Projects
+        </div>
+    )
+}
+
+export default projects
