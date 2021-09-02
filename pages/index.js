d05a364f84ce8549426e62dda1ec95ce7d44575f
sanket shevkar <sanket@sankets-MacBook-Air.local>
2021-09-02 21:48:49 +0530

    refactor(pages): about page changes
    Signed-off-by: sanket shevkar <sanket@sankets-MacBook-Air.local>

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
-import { Flex, WrapItem, Container, Wrap, Text } from '@chakra-ui/react';
-import ExternalLinkIcon from '@chakra-ui/icons';
+import { Flex } from '@chakra-ui/react';
 import Head from 'next/head';
-import Link from 'next/link';
 import _ from 'lodash';
-import Assets from '../components/About/Assets';
-import Description from '../components/About/Description';
-import Form from '../components/About/Form';
 
 
 function index(){
@@ -16,10 +11,6 @@ function index(){
         <meta property="og:title" content="My page title" key="title" />
         <link rel="icon" href="https://i.imgur.com/r84NYEK.png" />
       </Head>
-      {/* <Wrap>
-        <WrapItem><Container><Assets /></Container></WrapItem>
-        <WrapItem><Container maxW="container.md"><Description /><Form /></Container></WrapItem>
-      </Wrap> */}
     </Flex>
   )
 }
